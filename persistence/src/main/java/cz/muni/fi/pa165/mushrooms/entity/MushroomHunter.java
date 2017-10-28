0f2a93b9a8f32d4b39fd436b9631f3a5efe3bd53
Matus panik <xpanik2@fi.muni.cz>
2017-10-28 20:46:57 +0200

    new constrains

--- persistence/src/main/java/cz/muni/fi/pa165/mushrooms/entity/MushroomHunter.java
+++ persistence/src/main/java/cz/muni/fi/pa165/mushrooms/entity/MushroomHunter.java
@@ -1,12 +1,6 @@
 package cz.muni.fi.pa165.mushrooms.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +9,7 @@ import java.util.Set;
  * @author Buvko
  */
 @Entity
+@Table(uniqueConstraints = {@UniqueConstraint(columnNames = "userNickname")})
 public class MushroomHunter {
 
     @Id
@@ -34,6 +29,9 @@ public class MushroomHunter {
     Set<Visit> visits = new HashSet<>();
 
     private boolean isAdmin;
+
+    @Column(nullable=false, unique = true)
+    private String userNickname;
 
     @Column
     private String personalInfo;
@@ -97,17 +95,23 @@ public class MushroomHunter {
         if (this == o) return true;
         if (!(o instanceof MushroomHunter)) return false;
 
-        MushroomHunter mushroomHunter = (MushroomHunter) o;
+        MushroomHunter that = (MushroomHunter) o;
 
-        if (!getFirstName().equals(mushroomHunter.getFirstName())) return false;
-        if (!getSurname().equals(mushroomHunter.getSurname())) return false;
-        return getPersonalInfo() != null ? getPersonalInfo().equals(mushroomHunter.getPersonalInfo()) : mushroomHunter.getPersonalInfo() == null;
+        if (isAdmin() != that.isAdmin()) return false;
+        if (!getFirstName().equals(that.getFirstName())) return false;
+        if (!getSurname().equals(that.getSurname())) return false;
+        if (getVisits() != null ? !getVisits().equals(that.getVisits()) : that.getVisits() != null) return false;
+        if (!userNickname.equals(that.userNickname)) return false;
+        return getPersonalInfo() != null ? getPersonalInfo().equals(that.getPersonalInfo()) : that.getPersonalInfo() == null;
     }
 
     @Override
     public int hashCode() {
         int result = getFirstName().hashCode();
         result = 31 * result + getSurname().hashCode();
+        result = 31 * result + (getVisits() != null ? getVisits().hashCode() : 0);
+        result = 31 * result + (isAdmin() ? 1 : 0);
+        result = 31 * result + userNickname.hashCode();
         result = 31 * result + (getPersonalInfo() != null ? getPersonalInfo().hashCode() : 0);
         return result;
     }
