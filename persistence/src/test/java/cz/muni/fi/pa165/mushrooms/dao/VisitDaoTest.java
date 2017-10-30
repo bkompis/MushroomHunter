ea48643666729e353ab69ad910d957b7c11a7423
Bohdan Cvejn <bohdancvejn@gmail.com>
2017-10-30 19:08:02 +0100

    Second part of tests in VisitDaoTest

--- persistence/src/test/java/cz/muni/fi/pa165/mushrooms/dao/VisitDaoTest.java
+++ persistence/src/test/java/cz/muni/fi/pa165/mushrooms/dao/VisitDaoTest.java
@@ -1,13 +1,13 @@
 package cz.muni.fi.pa165.mushrooms.dao;
 
 import cz.muni.fi.pa165.mushrooms.entity.Forest;
+import cz.muni.fi.pa165.mushrooms.entity.Mushroom;
 import cz.muni.fi.pa165.mushrooms.entity.MushroomHunter;
 import cz.muni.fi.pa165.mushrooms.entity.Visit;
 import cz.muni.fi.pa165.mushrooms.validation.PersistenceSampleApplicationContext;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
@@ -18,13 +18,13 @@ import javax.persistence.PersistenceContext;
 import javax.validation.ConstraintViolationException;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 /**
- * @author
+ * @author BohdanCvejn
  */
 
 @ContextConfiguration(classes = PersistenceSampleApplicationContext.class)
@@ -95,8 +95,6 @@ public class VisitDaoTest extends AbstractTransactionalJUnit4SpringContextTests 
         Forest testForest = new Forest();
         testForest.setName("Test Forest");
 
-        MushroomHunter testHunter = new MushroomHunter();
-
         em.persist(testForest);
 
         Visit testVisit = new Visit();
@@ -110,7 +108,6 @@ public class VisitDaoTest extends AbstractTransactionalJUnit4SpringContextTests 
 
     @Test
     public void create_visitWithNullForest() throws Exception {
-        Forest testForest = new Forest();
         MushroomHunter testHunter = new MushroomHunter();
         testHunter.setFirstName("Maradona");
         testHunter.setSurname("Sandokan");
@@ -151,9 +148,237 @@ public class VisitDaoTest extends AbstractTransactionalJUnit4SpringContextTests 
         em.persist(testVisit);
 
         assertThat(visitDao.findAll().size()).isEqualTo(sizeBeforeCreate + 1);
-        System.out.println(visitDao.findAll().size());
         assertThat(visitDao.findAll()).containsExactlyInAnyOrder(visit1, visit2, testVisit);
+    }
+
+    @Test
+    public void create_visitWithNullDate() throws Exception {
+        Forest testForest = new Forest();
+        testForest.setName("Test Forest");
+
+        MushroomHunter testHunter = new MushroomHunter();
+        testHunter.setFirstName("Maradona");
+        testHunter.setSurname("Sandokan");
+        testHunter.setUserNickname("bidG");
+
+        em.persist(testForest);
+        em.persist(testHunter);
+
+        Visit testVisit = new Visit();
+        testVisit.setHunter(testHunter);
+        testVisit.setForest(testForest);
+        testVisit.setDate(null);
+
+        assertThatThrownBy(() -> visitDao.create(testVisit)).isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    public void update_validVisitDate() {
+        LocalDate newDate = LocalDate.of(2017,5,12);
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foundVisit.setDate(newDate);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getDate()).isEqualTo(newDate);
+    }
+
+    @Test
+    public void update_validVisitForestName() {
+        String forestNewName = "Sexy forest";
+        Forest foudForest = em.find(Forest.class, forest1.getId());
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foudForest.setName(forestNewName);
+
+        foundVisit.setForest(foudForest);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getForest().getName()).isEqualTo(forestNewName);
+    }
+
+    @Test
+    public void update_validVisitForestDescription() {
+        String forestNewDescription = "Dark and hard to find anything";
+        Forest foundForest = em.find(Forest.class, forest1.getId());
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foundForest.setDescription(forestNewDescription);
+
+        foundVisit.setForest(foundForest);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getForest().getDescription()).isEqualTo(forestNewDescription);
+    }
+
+    @Test
+    public void update_validVisitHunterName() {
+        String hunterNewName = "Harry";
+        MushroomHunter foudHunter = em.find(MushroomHunter.class, mushroomHunter1.getId());
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foudHunter.setFirstName(hunterNewName);
+
+        foundVisit.setHunter(foudHunter);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getHunter().getFirstName()).isEqualTo(hunterNewName);
+    }
+
+    @Test
+    public void update_validVisitHunterSurname() {
+        String hunterNewSurname = "Potter";
+        MushroomHunter foudHunter = em.find(MushroomHunter.class, mushroomHunter1.getId());
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foudHunter.setSurname(hunterNewSurname);
+
+        foundVisit.setHunter(foudHunter);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getHunter().getSurname()).isEqualTo(hunterNewSurname);
+    }
+
+    @Test
+    public void update_validVisitHunterNickName() {
+        String hunterNickName = "The boy who survived";
+        MushroomHunter foudHunter = em.find(MushroomHunter.class, mushroomHunter1.getId());
+        Visit foundVisit = em.find(Visit.class, visit1.getId());
+
+        foudHunter.setUserNickname(hunterNickName);
+
+        foundVisit.setHunter(foudHunter);
+        visitDao.update(foundVisit);
+        em.flush();
+
+        assertThat(em.find(Visit.class, visit1.getId()).getHunter().getUserNickname()).isEqualTo(hunterNickName);
+    }
+
+
+    @Test
+    public void update_nullVisit() {
+        assertThatThrownBy(() ->visitDao.update(null)).hasRootCauseInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void update_visitWithNullHunter() {
+        Forest testForest = new Forest();
+        testForest.setName("Test Forest");
+
+        em.persist(testForest);
+
+        Visit testVisit = new Visit();
+        testVisit.setHunter(null);
+        testVisit.setForest(testForest);
+        testVisit.setDate(LocalDate.of(2017,2,5));
+        testVisit.setNote("note for the forest");
+
+        assertThatThrownBy(() -> visitDao.update(testVisit)).isInstanceOf(ConstraintViolationException.class);
+    }
+
+    @Test
+    public void update_visitWithNullForest() {
+        MushroomHunter testHunter = new MushroomHunter();
+        testHunter.setFirstName("Maradona");
+        testHunter.setSurname("Sandokan");
+        testHunter.setUserNickname("bidG");
+
+        em.persist(testHunter);
+
+        Visit testVisit = new Visit();
+        testVisit.setHunter(testHunter);
+        testVisit.setForest(null);
+        testVisit.setDate(LocalDate.of(2017,2,5));
+        testVisit.setNote("note for the forest");
+
+        assertThatThrownBy(() -> visitDao.update(testVisit)).isInstanceOf(ConstraintViolationException.class);
 
     }
 
+    @Test
+    public void delete_validVisit() throws Exception {
+        int sizeBeforeCreate = visitDao.findAll().size();
+
+        visitDao.delete(visit1);
+        em.flush();
+
+        assertThat(visitDao.findAll().size()).isEqualTo(sizeBeforeCreate - 1);
+        assertThat(visitDao.findAll()).containsExactlyInAnyOrder(visit2);
+    }
+
+    @Test
+    public void delete_nullVisit() throws Exception {
+        assertThatThrownBy(() -> visitDao.delete(null)).hasRootCauseInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void delete_nonExistingVisit() {
+        int sizeBeforeCreate = visitDao.findAll().size();
+        Forest testForest = new Forest();
+        testForest.setName("Test Forest");
+
+        MushroomHunter testHunter = new MushroomHunter();
+        testHunter.setFirstName("Maradona");
+        testHunter.setSurname("Sandokan");
+        testHunter.setUserNickname("bidG");
+
+        em.persist(testForest);
+        em.persist(testHunter);
+
+        Visit testVisit = new Visit();
+        testVisit.setHunter(testHunter);
+        testVisit.setForest(testForest);
+        testVisit.setDate(LocalDate.of(2017,2,5));
+
+        visitDao.delete(testVisit);
+        em.flush();
+
+        assertThat(visitDao.findAll().size()).isEqualTo(sizeBeforeCreate);
+        assertThat(visitDao.findAll()).containsExactlyInAnyOrder(visit1,visit2);
+    }
+
+    @Test
+    public void findAll_visits() throws Exception {
+        assertThat(visitDao.findAll()).containsExactlyInAnyOrder(visit1,visit2);
+    }
+
+    @Test
+    public void findById_nullId() throws Exception {
+        assertThatThrownBy(() -> visitDao.findById(null)).hasRootCauseExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void findById_validId() throws Exception {
+        Visit testForest = visitDao.findById(visit1.getId());
+        assertThat(testForest).isNotNull();
+        assertThat(testForest).isEqualTo(visit1);
+    }
+
+    @Test
+    public void findById_invalidId() throws Exception {
+        Long invalidID = new Long(64000);
+        Visit testVisit = visitDao.findById(invalidID);
+        assertThat(testVisit).isNull();
+    }
+
+    @Test
+    public void findByDate_nullDateFrom() throws Exception {
+        assertThatThrownBy(() -> visitDao.findByDate(null,LocalDate.of(2017,10,25))).hasRootCauseExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void findByDate_nullDateTo() throws Exception {
+        assertThatThrownBy(() -> visitDao.findByDate(LocalDate.of(2017,10,25),null)).hasRootCauseExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    public void findByDate_validDate() throws Exception {
+        //I need help, my brain is dead by now
+    }
 }
