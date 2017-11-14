af8f10f3bb10a191d9a1bb44ea1af47788f96e21
bencikpeter <bencik.peter.ml@gmail.com>
2017-11-14 14:52:50 +0100

    interface implementations fixed, parameter name fixed

--- service/src/main/java/cz/muni/fi/pa165/mushrooms/service/facade/VisitFacadeImpl.java
+++ service/src/main/java/cz/muni/fi/pa165/mushrooms/service/facade/VisitFacadeImpl.java
@@ -1,6 +1,11 @@
 package cz.muni.fi.pa165.mushrooms.service.facade;
 
+import cz.muni.fi.pa165.mushrooms.dto.ForestDTO;
+import cz.muni.fi.pa165.mushrooms.dto.MushroomHunterDTO;
+import cz.muni.fi.pa165.mushrooms.dto.VisitDTO;
 import cz.muni.fi.pa165.mushrooms.facade.VisitFacade;
+
+import java.util.List;
 
 /**
  * TODO: create  javadoc
@@ -8,4 +13,33 @@ import cz.muni.fi.pa165.mushrooms.facade.VisitFacade;
  * @author Barbora Kompisova
  */
 public class VisitFacadeImpl implements VisitFacade {
+    @Override
+    public VisitDTO findById(String name) {
+        return null;
+    }
+
+    @Override
+    public void deleteVisit(VisitDTO visit) {
+
+    }
+
+    @Override
+    public void updateVisit(VisitDTO visit) {
+
+    }
+
+    @Override
+    public void createVisit(VisitDTO visit) {
+
+    }
+
+    @Override
+    public List<VisitDTO> listAllVisitsForForest(ForestDTO forest) {
+        return null;
+    }
+
+    @Override
+    public List<VisitDTO> listAllVisitsForMushromHunter(MushroomHunterDTO mushroomHunter) {
+        return null;
+    }
 }
