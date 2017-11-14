0c51bec586370916ea20fdd2d03cd87bea072875
bencikpeter <bencik.peter.ml@gmail.com>
2017-11-14 14:44:18 +0100

    Facade interfaces created

--- api/src/main/java/cz/muni/fi/pa165/mushrooms/facade/ForestFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/mushrooms/facade/ForestFacade.java
@@ -1,9 +1,30 @@
 package cz.muni.fi.pa165.mushrooms.facade;
 
+import cz.muni.fi.pa165.mushrooms.dto.ForestDTO;
+import cz.muni.fi.pa165.mushrooms.dto.MushroomDTO;
+import cz.muni.fi.pa165.mushrooms.dto.VisitDTO;
+
+import java.util.List;
+
 /**
  * TODO: create  javadoc
  *
- * @author Barbora Kompisova
+ * @author bencikpeter, bohdancvejn, bkompis, Lindar84, Buvko
  */
 public interface ForestFacade {
+
+    ForestDTO findByName(String name);
+
+    ForestDTO findById(String name);
+
+    ForestDTO findByMushroom(MushroomDTO mushroom);
+
+    void deleteForest(ForestDTO forest);
+
+    void updateForest(ForestDTO forest);
+
+    void createForest(ForestDTO forest);
+
+    List<ForestDTO> listAllForestWithMushroom(MushroomDTO mushroom); //complicated business function
+
 }
