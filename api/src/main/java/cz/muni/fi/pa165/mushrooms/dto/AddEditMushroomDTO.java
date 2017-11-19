4b5a68535e6c28fee0efc0e642f370e5a1b9dd85
Matus panik <xpanik2@fi.muni.cz>
2017-11-19 13:35:26 +0100

    mushroom DTOs

---
+++ api/src/main/java/cz/muni/fi/pa165/mushrooms/dto/AddEditMushroomDTO.java
@@ -0,0 +1,63 @@
+package cz.muni.fi.pa165.mushrooms.dto;
+
+import cz.muni.fi.pa165.mushrooms.enums.MushroomType;
+import javax.validation.constraints.NotNull;
+
+/**
+ * Created by Matúš on 18.11.2017.
+ */
+public class AddEditMushroomDTO {
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private MushroomType type;
+
+    @NotNull
+    private String intervalOfOccurrence;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MushroomType getType() {
+        return type;
+    }
+
+    public void setType(MushroomType type) {
+        this.type = type;
+    }
+
+    public String getIntervalOfOccurrence() {
+        return intervalOfOccurrence;
+    }
+
+    public void setIntervalOfOccurrence(String intervalOfOccurrence) {
+        this.intervalOfOccurrence = intervalOfOccurrence;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MushroomDTO)) return false;
+
+        MushroomDTO that = (MushroomDTO) o;
+
+        if (!getName().equals(that.getName())) return false;
+        if (getType() != that.getType()) return false;
+        return getIntervalOfOccurrence().equals(that.getIntervalOfOccurrence());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getType().hashCode();
+        result = 31 * result + getIntervalOfOccurrence().hashCode();
+        return result;
+    }
+}
--- api/src/main/java/cz/muni/fi/pa165/mushrooms/dto/MushroomDTO.java
+++ api/src/main/java/cz/muni/fi/pa165/mushrooms/dto/MushroomDTO.java
@@ -1,9 +1,64 @@
 package cz.muni.fi.pa165.mushrooms.dto;
 
+import cz.muni.fi.pa165.mushrooms.enums.MushroomType;
+
 /**
  * TODO: create  javadoc
  *
- * @author Barbora Kompisova
+ * @author Buvko
  */
 public class MushroomDTO {
+
+    private Long id;
+    private String name;
+    private MushroomType type;
+    private String intervalOfOccurrence;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public MushroomType getType() {
+        return type;
+    }
+
+    public void setType(MushroomType type) {
+        this.type = type;
+    }
+
+    public String getIntervalOfOccurrence() {
+        return intervalOfOccurrence;
+    }
+
+    public void setIntervalOfOccurrence(String intervalOfOccurrence) {
+        this.intervalOfOccurrence = intervalOfOccurrence;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MushroomDTO)) return false;
+
+        MushroomDTO that = (MushroomDTO) o;
+
+        if (!getName().equals(that.getName())) return false;
+        if (getType() != that.getType()) return false;
+        return getIntervalOfOccurrence().equals(that.getIntervalOfOccurrence());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getName().hashCode();
+        result = 31 * result + getType().hashCode();
+        result = 31 * result + getIntervalOfOccurrence().hashCode();
+        return result;
+    }
 }
