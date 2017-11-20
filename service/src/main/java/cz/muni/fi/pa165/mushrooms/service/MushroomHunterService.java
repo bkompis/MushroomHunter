f52ed9d52e0ac0a29f6be0e68b46196a081b7ea1
Barbora Kompišová <barbora.kompisova@gmail.com>
2017-11-20 14:18:54 +0100

    implements MushroomHunter facade & service

--- service/src/main/java/cz/muni/fi/pa165/mushrooms/service/MushroomHunterService.java
+++ service/src/main/java/cz/muni/fi/pa165/mushrooms/service/MushroomHunterService.java
@@ -1,9 +1,23 @@
 package cz.muni.fi.pa165.mushrooms.service;
+import cz.muni.fi.pa165.mushrooms.entity.MushroomHunter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
- * TODO: create  javadoc
+ * Service interface for operations concerning mushroom hunters, users of the system.
  *
- * @author Barbora Kompisova
+ * @author bkompis
  */
+@Service
 public interface MushroomHunterService {
+    MushroomHunter findHunterById(Long id);
+    MushroomHunter findHunterByNickname(String nickname);
+    void registerHunter(MushroomHunter hunter, String password);
+    void deleteHunter(MushroomHunter hunter);
+    void updateHunter(MushroomHunter hunter);
+    boolean updatePassword(MushroomHunter hunter, String oldPassword, String newPassword);
+    List<MushroomHunter> findAllHunters();
+    boolean authenticate(MushroomHunter hunter, String password);
+    boolean isAdmin(MushroomHunter hunter);
 }
