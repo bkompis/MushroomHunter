c0319a8a268f3506c14e3568bfe3562e994763b4
Barbora Kompišová <barbora.kompisova@gmail.com>
2018-01-06 10:54:34 +0100

    removed infinite recursion

--- rest/src/main/java/cz/muni/fi/pa165/mushrooms/rest/mixins/HunterDTOMixin.java
+++ rest/src/main/java/cz/muni/fi/pa165/mushrooms/rest/mixins/HunterDTOMixin.java
@@ -1,8 +1,13 @@
 package cz.muni.fi.pa165.mushrooms.rest.mixins;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 @JsonIgnoreProperties({ "passwordHash"})
+@JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "id")
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class HunterDTOMixin {
 
 }
